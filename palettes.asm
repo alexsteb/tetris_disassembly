d3f66e0e151782b90a03c44ec1e5477897d45309
alexsteb <40014112+alexsteb@users.noreply.github.com>
2018-06-07 16:01:20 +0900

    Update palettes.asm

--- palettes.asm
+++ palettes.asm
@@ -1,2 +1,2 @@
-rPALETTE_1  EQU %11100100   ;00 = white, 01 = light grey, 10 = dark grey, 11 = black (read RTL)
-rPALETTE_2  EQU %11000100   ;white, light grey, white, black
+PALETTE_1  EQU %11100100   ;00 = white, 01 = light grey, 10 = dark grey, 11 = black (read RTL)
+PALETTE_2  EQU %11000100   ;white, light grey, white, black
